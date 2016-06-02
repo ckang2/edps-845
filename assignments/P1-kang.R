c22d627a4c42b5d2e092d1a834794e4a355076d8
ckang2 <coldstone78@gmail.com>
2016-06-01 23:38:34 -0500

    P1-kang.R

--- assignments/P1-kang.R
+++ assignments/P1-kang.R
@@ -1,65 +1,105 @@
 # EDPS 845, P1
 # Chansuk Kang
 
-# function for computing analytic standard error (the delta method) of 
-# linear equating using random group desing
+# Function for computing analytic standard error (the delta method) of 
+# linear equating using random group design
 
 #################################################################3
 
 # data prep
-setwd("C:/Users/Chansuk/Desktop/edps845/edps-845/assignments")
-data("ACTmath")
 library("equate")
+library("devtools")
+#devtools::document()
+data("ACTmath")
 
 # Frequency tables of random group equating
 x <- as.freqtab(ACTmath[, 1:2])
 y <- as.freqtab(ACTmath[, c(1, 3)])
 
 # function 
-lin_rg <- function (x, y, ...){
-
-     xscale <- scales(x)
+linear_rg <- function (x, y, ...) {
+    if (missing(y))
+    y <- margin.table(x, 2)
+    if (margins(y) < margins(x))
+    x <- margin.table(x, 1)
+    xscale <<- scales(x)
 
   #use stats to get linear equating(revised from equate::linear) 
-      sigmax <- sd.freqtab(x)
-      sigmay <- sd.freqtab(y)
-      mux <- mean(x)
-      muy <- mean(y)
-      slope <- sigmay/sigmax
-      intercept <- muy - slope*mux
+      sigmax <<- sd.freqtab(x)
+      sigmay <<- sd.freqtab(y)
+      mux <<- mean(x)
+      muy <<- mean(y)
+      slope <<- sigmay/sigmax
+      intercept <<- muy - slope*mux
 
-    lyx <- intercept + slope*xscale
+    yx <<- intercept + slope*xscale
+    #yx <<- lin(xscale, intercept, slope)
    
-  # get se
-    sigmaysq <-var.freqtab(y)
-    sux <- summary(x)
-    nx <- sux$n
-    suy <- summary(y)
-    ny <- suy$n
-    skx <- skew.freqtab(x)
-    sky <- skew.freqtab(y)
-    kux <- kurt.freqtab(x)
-    kuy <- kurt.freqtab(y)
+  # Get se
+    sigmaysq <<-var.freqtab(y)
+    sux <<- summary(x)
+    nx <<- sux$n
+    suy <<- summary(y)
+    ny <<- suy$n
+    skx <<- skew.freqtab(x)
+    sky <<- skew.freqtab(y)
+    kux <<- kurt.freqtab(x, margin = 1)
+    kuy <<- kurt.freqtab(y, margin = 1)
     
-    sk <- (skx/nx + sky/ny)
-    zs <- (xscale - mux)/sigmax
-    ku <- (kux -1)/4*nx + (kuy - 1)/4*ny
+    sk<<- ((skx/nx) + (sky/ny))
+    zs <<- (xscale - mux)/sigmax
+    ku <<- ((kux - 1)/(4*nx)) + ((kuy - 1)/(4*ny))
+
+    selrg <<- function(nx, ny, sk, ku, zs) {
+  
+    sigmaysq * ((1/nx) + (1/ny) + (sk * zs) + (ku * zs^2))
+      
+    }
+    se <<- selrg(nx, ny, sk, ku, zs)
     
-    lse <- sigmaysq * (1/nx + 1/ny + sk * zs
-          + ku * zs ^ 2)
-    # if x and y are assumed normally distributed
-    lse1 <- (sigmaysq/2) * (1/nx + 1/ny) * (2 + zs ^ 2)
+   # if x and y are assumed normally distributed
+    selrg1 <<- function(nx, ny, zs) {
+      
+    (sigmaysq/2) * ((1/nx) + (1/ny)) * (2 + (zs^2))
+      
+    }
+    
+    se1 <<- selrg1(nx, ny, zs)
+    
     # if sample size for x and y are assumed to be equal
-    lse2 <- ((2*sigmaysq)/(nx + ny)) * (2 + zs ^ 2)
+    selrg2 <<- function(nx, ny, nt, zs) {
     
-  out <- cbind(lyx, lse, lse1, lse2)
+    nt <<- nx + ny  
+    ((2*sigmaysq)/(nt)) * (2 + (zs^2))
+      
+    }
+    
+    se2 <<- selrg2(nx, ny, nt, zs)
+    
+  out <- cbind(yx, se, se1, se2)
+  #out <- list(yx = yx, se = se, se1 = se1, se2 = se2)
   return(out)
+
 }
+
+# checking results
+round(se, 2)
+round(se1, 2)
+round(se2, 2)
+round(mean(se), 2)
+round(mean(se1), 2)
+round(mean(se2), 2)
+
+plot(xscale, yx)
+plot(xscale, se)
+plot(xscale, se1)
+plot(xscale, se2)
 
 # Comparison to bootstrap standard error (from equate::equate)
 eql <- equate(x, y, type = "l", method = "none", boot = T, reps = 100)
 #bootstrap standard error
 eql$boot$se
+plot(eql$boot$se)
 round(eql$boot$se, 2)
 round(mean(eql$boot$se), 2)
 
